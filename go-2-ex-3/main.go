bf2386c97a4ecc49e6629fc089c475f3629a67bd
janishuber <janis.huber@itme.ch>
2025-11-21 14:36:08 +0100

    Aufgabe06

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -4,13 +4,21 @@ import "fmt"
 
 func main() {
 	// TODO: create a map called "modules"
+	modules := map[int]string{
+		104: "Blabla1",
+		117: "Blabla2",
+		346: "Cloud Computing",
+	}
 
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
 	// TODO: delete one
+	delete(modules, 104)
 	// TODO: add one
+	modules[104] = "Blabla1"
 	// TODO: replace one
+	modules[117] = "Blabla2 - Replaced"
 	fmt.Println(modules)
 }
