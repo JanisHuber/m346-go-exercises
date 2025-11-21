0d9a90e0157585ecac1b7cc161620867e734f890
janishuber <janis.huber@itme.ch>
2025-11-21 14:13:32 +0100

    Aufgabe01

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: Declare and initialize the variables being used in the output!
+	var firstName string = "Janis"
+	var lastName string = "Huber"
+	var dayOfBirth int = 15
+	var monthOfBirth int = 9
+	var yearOfBirth int = 2007
+	var numberOfSiblings int = 1
+	var heightInMeters float32 = 1.70
+	var zodiacSign byte = 'J'
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
