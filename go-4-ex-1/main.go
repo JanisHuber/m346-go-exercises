737fe185bfa3cf7e39756cdc50bd011e0b7d5e1e
janishuber <janis.huber@itme.ch>
2025-12-05 14:04:00 +0100

    Aufgabe12

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"log"
+)
+
 // TODO: implement the function computeGrade
+func computeGrade(gotPoints float32, maxPoints float32) float32 {
+	if gotPoints > maxPoints {
+		panic("max points exceeded")
+	}
+	return 5/maxPoints*gotPoints + 1
+}
 
 func main() {
 	// TODO: call the function computeGrade
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("panic occurred:", err)
+		}
+	}()
+	var grade float32 = computeGrade(21, 20)
+	// Why is it such a complicated expression instead of just a try and catch block?
+	// And which languages named it exeptions "panic" hahaha
+	fmt.Printf("%.2f\n", grade)
 }
